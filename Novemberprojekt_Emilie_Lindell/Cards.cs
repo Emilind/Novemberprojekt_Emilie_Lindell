d820e886df17db4e5e8c8a1a557d95d3b941473b
Emilind <emilie.lindell@elev.ga.ntig.se>
2019-10-23 14:57:13 +0200

    Intital commit
    Jag har valt att göra ett kortspel kallat "war".  Jag har enbart börjat med klassen  Cards lite enkelt, inte så mycket.

--- Novemberprojekt_Emilie_Lindell/Cards.cs
+++ Novemberprojekt_Emilie_Lindell/Cards.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Novemberprojekt_Emilie_Lindell
 {
-    class Cards
+    public enum Suit// Jag använder enumernation för att det ska användas för de olika bestämda och namngivna typerna av korten tex hjärter & spader osv.
+    {
+        Klöver, Ruter, Spader, Hjärter,
+    }
+    public class Cards
     {
     }
 }
